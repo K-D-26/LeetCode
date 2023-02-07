44ce57443f06e093ba00aec02097417e8cae5510
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-02-07 07:57:01 +0530

    Update 198)House_Robber.java

--- 198)House_Robber.java
+++ 198)House_Robber.java
@@ -1,3 +1,5 @@
+// Method 1 (Memoization):
+
 class Solution {
     int[] memo;
     public int rob(int[] nums) {
@@ -13,5 +15,30 @@ class Solution {
             return memo[n];
         memo[n] = Math.max(rob(nums, n-2) + nums[n], rob(nums, n-1));
         return memo[n];
+    }
+}
+
+
+
+// Method 2 (Tabulation + space optimized):
+
+class Solution {
+    public int rob(int[] nums) {
+        // Tabulation (space optimized)
+        
+        int n = nums.length;
+        int prev2 = 0, prev = nums[0];
+        
+        for(int i=1; i<n; i++) {
+            int rob = nums[i];
+            if(i > 1)
+                rob += prev2;
+            int notRob = prev;
+            int curr = Math.max(rob, notRob);            
+            prev2 = prev;
+            prev = curr;
+        }
+        
+        return prev;
     }
 }
