de7233787c1ef53f4538fdd01646e7d94d63300b
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-06-07 23:47:35 +0530

    Update 931)Minimum_Falling_Path_Sum.java

--- 931)Minimum_Falling_Path_Sum.java
+++ 931)Minimum_Falling_Path_Sum.java
@@ -1,3 +1,35 @@
+// Method 1 (Tabulation DP):
+
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                if(i == n-1)
+                    dp[i][j] = matrix[i][j];
+                else if(j == 0)
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j], dp[i+1][j+1]);
+                else if(j == n-1)
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j], dp[i+1][j-1]);
+                else
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j-1], Math.min(dp[i+1][j], dp[i+1][j+1]));
+            }
+        }
+        
+        int ans = Integer.MAX_VALUE;
+        for(int j=0; j<n; j++)
+            ans = Math.min(ans, dp[0][j]);
+        
+        return ans;
+    }
+}
+
+
+
+// Method 2:
+
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
