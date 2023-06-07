7f90f79d42755895013dcdbb99d80127ea064763
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-06-07 11:33:25 +0530

    Update 63)Unique_Paths_II.java

--- 63)Unique_Paths_II.java
+++ 63)Unique_Paths_II.java
@@ -1,3 +1,5 @@
+// Method 1: 
+
 class Solution {
     public int uniquePathsWithObstacles(int[][] mat) {
         // Tabulation (space optimised)
@@ -25,5 +27,34 @@ class Solution {
         }
         
         return prev[m-1];
+    }
+}
+
+
+
+// Method 2: (Easily understandable and optimized using DP)
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int[][] dp = new int[m][n];
+        
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                if(arr[i][j] == 1)
+                    dp[i][j] = 0;
+                else if(i == m-1 && j == n-1)
+                    dp[i][j] = 1;
+                else if((i == m-1 && dp[i][j+1] == 0) || (j == n-1 && dp[i+1][j] == 0))
+                    dp[i][j] = 0;
+                else if(i == m-1 || j == n-1)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        
+        return dp[0][0];
     }
 }
