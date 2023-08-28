3f6e216c0024d4a867d286c561324ea94d85a0c0
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-08-28 15:53:10 +0530

    Update 94) Binary Tree Inorder Traversal.java

--- 94) Binary Tree Inorder Traversal.java
+++ 94) Binary Tree Inorder Traversal.java
@@ -1,3 +1,5 @@
+// Method 1: (Recursion)
+
 class Solution {
     List<Integer> li = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -11,5 +13,44 @@ class Solution {
         inorder(node.left);
         li.add(node.val);
         inorder(node.right);
+    }
+}
+
+
+// Method 2: (Morris Traversal)
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // Morris Traversal
+        // Time: O(n), Space: O(1)
+        
+        TreeNode cur = root;
+        List<Integer> ans = new ArrayList<>();
+        
+        while(cur != null) {
+            if(cur.left == null) {
+                ans.add(cur.val);
+                cur = cur.right;
+            }
+            else {
+                TreeNode lastRight = cur.left;      // last right node in the left sub-tree of cur
+                while(lastRight.right != null && lastRight.right != cur) {
+                    lastRight = lastRight.right;
+                }
+                // connect last right node of left sub-tree to it's root.
+                if(lastRight.right == null) {
+                    lastRight.right = cur;
+                    cur = cur.left;
+                }
+                // after iterating the left part, break the connection.
+                else {
+                    lastRight.right = null;
+                    ans.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+        
+        return ans;
     }
 }
